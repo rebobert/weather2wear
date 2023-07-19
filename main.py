8581852065fd5f19bb60a28826c43951f9f39e5a
rebobert <47259470+rebobert@users.noreply.github.com>
2023-07-19 13:10:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -358,7 +358,7 @@ def findBestItem(itemList, avgTemp):
         # check if item is none value, if not, print to user. 
         bestItemName = bestItem.get('name')
         if not bestItemName == None:
-            itemURL = f'https://www.google.com/search?tbm=shop&hl=en&psb=1&ved=2ahUKEwi3rIOgwIyAAxXgzsIEHaFCArMQu-kFegQIABAK&q={BestItemName}&sclient=products-cc'
+            itemURL = f'https://www.google.com/search?tbm=shop&hl=en&psb=1&ved=2ahUKEwi3rIOgwIyAAxXgzsIEHaFCArMQu-kFegQIABAK&q={bestItemName}&sclient=products-cc'
             st.write("[Item Link](%s)" % itemURL)  
             st.markdown("[Item Link](%s)" % itemURL)
       
