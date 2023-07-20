2e94894fe019b0fb02b05e384a0f4d6a0687f9d1
rebobert <47259470+rebobert@users.noreply.github.com>
2023-07-20 12:53:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def main():
   with left:
       st.image('./PoweredByVC-WeatherLogo-RoundedRect.png', width=100)
       st.markdown("[![Foo]('./PoweredByVC-WeatherLogo-RoundedRect.png')](https://www.visualcrossing.com/weather-api)")
-
+      st.markdown(![Foo](./PoweredByVC-WeatherLogo-RoundedRect.png))
 
 # Call the weather API from visual crossing for the current weather data. 
 def apiCall(currentCity):
@@ -295,7 +295,7 @@ def getItemTemp(tempDifference, itemType, clothingData, avgTemp):
     list = []
     # check if values 
     for item in clothingData[itemType]:
-        if int(clothingData[itemType][n]['minTemp']) <= avgTemp + tempDifference/2 <= int(clothingData[itemType][n]['maxTemp']):
+        if int(clothingData[itemType][n]['minTemp']) <= avgTemp <= int(clothingData[itemType][n]['maxTemp']):
             min = int(item.get('minTemp'))
             max = int(item.get('maxTemp'))
             list.append(item)
