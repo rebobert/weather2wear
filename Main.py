861094ae49f67de1e553e46bbf5aea4c8ec3cf71
Steven Tworow <47259470+rebobert@users.noreply.github.com>
2023-07-26 13:05:50 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -49,6 +49,19 @@ def background(image_file):
     )
 background('rb.png')
 
+#Remove link icon beside each text
+st.markdown("""
+    <style>
+    /* Hide the link button */
+    .stApp a:first-child {
+        display: none;
+    }
+    .css-15zrgzn {display: none}
+    .css-eczf16 {display: none}
+    .css-jn99sy {display: none}
+    </style>
+
+    """, unsafe_allow_html=True)
 # main function calls all other functions and collects data
 def main():
   st.markdown("<h1 style='text-align: center; font-size:100px;'><span style='color: orange;'>&#x2600;</span></h1>", unsafe_allow_html=True)     
